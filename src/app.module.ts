082aab5ba937d526dc4d65f6a7a3d451c4cf82b0
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-07-03 11:47:22 +0530

    Introducing customer package

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
