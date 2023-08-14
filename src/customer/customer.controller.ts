e59ef9f017fd25867c9cce9384d915d04199238d
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-14 18:07:50 +0530

    introducing save customer in customer controller

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,36 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { CreateCustomerDto } from './dto/create.customer.dto';
-import { UpdateCustomerDto } from './dto/update.customer.dto';
+import { Customer } from './interfaces/customer.interface';
 
 @Controller('customer')
 export class CustomerController {
   constructor(private readonly customerService: CustomerService) {}
 
   @Post()
-  create(@Body() createCustomerDto: CreateCustomerDto) {
-    return this.customerService.create(createCustomerDto);
-  }
-
-  @Get()
-  findAll(): string  { 
-    // return this.customerService.findAll();
-    return 'Get all customers';
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    // return this.customerService.findOne(+id);
-    return `Item ${id} found`;
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-    return this.customerService.update(+id, updateCustomerDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.customerService.remove(+id);
+  async create(
+    @Body() createCustomerDto: CreateCustomerDto,
+  ): Promise<Customer> {
+    return this.customerService.saveCustomer(createCustomerDto);
   }
 }
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create.customer.dto';
 import { UpdateCustomerDto } from './dto/update.customer.dto';
+import { Customer } from './interfaces/customer.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CustomerService {
-  create(createCustomerDto: CreateCustomerDto) {
-    return 'This action adds a new customer';
-  }
+  constructor(
+    @InjectModel('Customer') private readonly customerModel: Model<Customer>,
+  ) {}
 
-  findAll() {
-    return `This action returns all customer`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} customer`;
-  }
-
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} customer`;
-  }
 }
