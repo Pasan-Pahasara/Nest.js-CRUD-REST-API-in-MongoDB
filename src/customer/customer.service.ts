b038c2d3b5326e000f47b13390582642c909186b
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-14 18:19:51 +0530

    refactored customer service

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCustomerDto } from './dto/create.customer.dto';
-import { UpdateCustomerDto } from './dto/update.customer.dto';
 import { Customer } from './interfaces/customer.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
